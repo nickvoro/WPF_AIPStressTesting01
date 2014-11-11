a96e8a56aa1dfb70480264caa4a4370d9d16e35d
nickvoro <nick1962@mail.ru>
2014-11-11 18:08:31 +0600

    Доработал общую схему старта и останова потока (thread) тестирования (пока без наполнения кодом самого потока)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,8 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Xceed.Wpf.Toolkit;
 using System.Text.RegularExpressions;
+using MessageBox = Xceed.Wpf.Toolkit.MessageBox;
 
 namespace WPF_AIPStressTesting01
 {
@@ -24,11 +27,14 @@ namespace WPF_AIPStressTesting01
     public const int MachineQuantityMax = 100;
     public const int TimeScaleFactorMax = 1000;
 
+    private Thread _thread;
+    private volatile bool _threadStarted;
+
     public MainWindow()
     {
       InitializeComponent();
       DataGridMachines.ItemsSource = Machine.GetMachines();
-        DataGridStates.ItemsSource = State.GetStates();
+      DataGridStates.ItemsSource = State.GetStates();
     }
 
     // ButtonSpinner SpinnerMachineQuantity
@@ -46,7 +52,7 @@ namespace WPF_AIPStressTesting01
         value--;
 
       if (value < 1)
-        value = MachineQuantityMax;
+        value = 1;
       else if (value > MachineQuantityMax)
         value = MachineQuantityMax;
 
@@ -78,8 +84,11 @@ namespace WPF_AIPStressTesting01
       int i;
       var value = !int.TryParse(txtBox.Text, out i) ? MachineQuantityMax : i;
 
-      if (value == 0)
+      if (value == 0 || value == MachineQuantityMax)
+      {
+        txtBox.Text = value.ToString();
         return;
+      }
 
       if (value < 1 || value > MachineQuantityMax)
       {
@@ -154,8 +163,11 @@ namespace WPF_AIPStressTesting01
       double d;
       var value = !double.TryParse(txtBox.Text, out d) ? TimeScaleFactorMax : d;
 
-      if (value == 0)
+      if (value == 0 || value == TimeScaleFactorMax)
+      {
+        txtBox.Text = value.ToString();
         return;
+      }
 
       if (value <= 0 || value > TimeScaleFactorMax)
       {
@@ -183,12 +195,71 @@ namespace WPF_AIPStressTesting01
 
     private void ButtonStart_Click(object sender, RoutedEventArgs e)
     {
+      int mq = _MachineQuantity();
+      if (mq <= 0)
+      {
+        MessageBox.Show("Parameter Machine Quantity is not setted!");
+        return;
+      }
 
+      double tsf = _TimeScaleFactor();
+      if (tsf <= 0)
+      {
+        MessageBox.Show("Parameter Time Scale Factor is not setted!");
+        return;
+      }
+
+      _thread = new Thread(Test);
+      _threadStarted = true;
+      _thread.Start();
+      ButtonStart.IsEnabled = false;
+      ButtonStop.IsEnabled = true;
     }
 
     private void ButtonStop_Click(object sender, RoutedEventArgs e)
     {
-
+      _threadStarted = false;
+      ButtonStop.IsEnabled = false;
+      _NestedEmptyMessageLoop();
+      if ((_thread != null)) while (_thread.Join(1000)) {};
+      ButtonStart.IsEnabled = true;
     }
+
+    // used functions
+
+    // see about trick at link: http://www.codeproject.com/Articles/271598/Application-DoEvents-in-WPF
+    private void _NestedEmptyMessageLoop()
+    {
+      Application.Current.Dispatcher.Invoke(
+        DispatcherPriority.Background,
+        new ThreadStart(delegate { })
+        );
+    }
+    private int _MachineQuantity()
+    {
+      var spinner = SpinnerMachineQuantity;
+      var txtBox = (TextBox)spinner.Content;
+      int i;
+      var value = !int.TryParse(txtBox.Text, out i) ? 0 : i;
+      return value;
+    }
+    private double _TimeScaleFactor()
+    {
+      var spinner = SpinnerTimeScaleFactor;
+      var txtBox = (TextBox)spinner.Content;
+      double d;
+      var value = !double.TryParse(txtBox.Text, out d) ? 0 : d;
+      return value;
+    }
+
+    private void Test()
+    {
+        while (_threadStarted)
+        {
+          Thread.Sleep(TimeSpan.FromSeconds(4));
+          Thread.Sleep(TimeSpan.FromSeconds(4));
+        }
+    }
+  
   }
 }
